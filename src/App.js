e96860dd12de80963e0f41f509604f648616ec7a
Christopher Adams <chrismichaeladams@gmail.com>
2019-04-21 11:10:18 -0400

    Initial commit, create react app, initially display dupes and non-dupes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFileChosen = this.handleFileChosen.bind(this);
+
+    this.state = {
+      csvFile: ""
+    }
+  }
+
+  /*
+   * Reads in the csv file for processing
+   */
+  handleFileChosen(file) {
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+      let text = fileReader.result;
+
+      var textCommaSplit = text.split('\n');
+      //console.log('textCommaSplit', textCommaSplit);
+
+      // Stor the headings:
+      let dataHeading = textCommaSplit[0];
+      console.log('**************************dataHeading****************', dataHeading);
+
+      let emailAll = [];
+      let dupeArray = [];
+      let nonDupeArray = [];
+
+      for (var j = 1; j < textCommaSplit.length; j++) {
+
+        // Regex, exclude splitting on double quotes with comma
+        // Handles case where data has a comma in data that is not the separator.
+        var tempRow = textCommaSplit[j].match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+
+        emailAll.push(
+          {
+            "email": tempRow[4],
+            "data": tempRow
+          }
+        );
+
+        let tempArray = emailAll.filter((count) => {
+          return count.email === tempRow[4];
+        });
+
+        if (tempArray.length > 1) {
+          dupeArray.push(tempArray);
+        } else {
+          nonDupeArray.push(tempArray);
+        }
+      }
+
+
+      //console.log('emailAll', emailAll);
+      console.log('dupeArray', JSON.stringify(dupeArray));
+      console.log('nonDupeArray', JSON.stringify(nonDupeArray));
+
+      this.setState({
+        dupes: JSON.stringify(dupeArray),
+        nonDupes: JSON.stringify(nonDupeArray)
+      });
+    }
+    fileReader.readAsText(file);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <form method="post">
+        <input 
+          id="csvinput" 
+          type="file" 
+          accept=".csv" 
+          name="csvinput"
+          onChange={e => this.handleFileChosen(e.target.files[0])}
+        >
+        </input>
+        <button>Submit</button>
+      </form>
+
+      <h1>Duplicates</h1>
+      {this.state.dupes}
+      <br></br>
+      <br></br>
+      <h1>Non-Duplicates</h1>
+      {this.state.nonDupes}
       </div>
     );
   }
